0179d95c51bf044b5e2ad4844babb3860c4aab75
Krista Nittmann <cieloangel@cox.net>
2021-05-13 11:47:06 -0500

    Added DonationService.java and DonationServiceImpl.java.

---
+++ src/main/java/com/group3/DonationManagementSystem/service/DonationService.java
@@ -0,0 +1,9 @@
+package com.group3.DonationManagementSystem.service;
+
+import com.group3.DonationManagementSystem.model.Donation;
+
+import java.util.List;
+
+public interface DonationService {
+    List<Donation> getAllDonations();
+}
